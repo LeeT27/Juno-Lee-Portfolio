23b459ef1294f939ce6054f78ba887147b22d63f
LeeT27 <138246438+LeeT27@users.noreply.github.com>
2024-08-16 08:53:59 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Module1 from './components/Module1.jsx';
+
+function App() {
+  return (
+    <>
+      <Module1></Module1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Module1.jsx
@@ -0,0 +1,16 @@
+import './Module1.css';
+import TypedText from './TypedText';
+function Module1() {
+  return (
+    <>
+      <div id="module1Container">
+        <div id="contentBox">
+          <div id="typed-text">
+            <TypedText />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Module1;
